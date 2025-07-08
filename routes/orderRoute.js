724292b64711cf7edae2a47f6ee5336a7132af5c
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-07-07 21:08:02 -0700

    Fix : Cart Controller and OrderController

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../middlewares/authMiddleware");
-const { createOrGetCustomer, verifyAndPlaceOrder } = require('../controllers/orderController');
+const { createOrGetCustomer } = require('../controllers/orderController');
 
 router.post('/', authenticate, createOrGetCustomer);
-router.post('/verify', verifyAndPlaceOrder);
 
 module.exports = router;
